acce3ab717102706e46b7563f20ee22f15cb4462
Thomas Breuss <thomasbreuss@gmx.ch>
2015-08-09 15:07:18 +0200

    Add Imagine lib manually, no composer dependency

--- ImaginePlugin.php
+++ ImaginePlugin.php
@@ -19,6 +19,11 @@ class ImaginePlugin extends Herbie\Plugin
 
     public function onTwigInitialized(Herbie\Event $event)
     {
+        // Add custom namespace path to Imagine lib
+        $vendorDir = $this->getService('Config')->get('site.path') . '/../vendor';
+        $autoload = require($vendorDir . '/autoload.php');
+        $autoload->add('Imagine', __DIR__ . '/vendor/lib');
+
         $config = $this->getService('Config');
         $basePath = $this->getService('Request')->getBasePath();
         $event['twig']->addExtension(new ImagineExtension($config, $basePath));
